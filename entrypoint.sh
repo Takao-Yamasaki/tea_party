f3a3204551d2864a38f52efa5ef3e996f4c806af
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-09-15 20:05:00 +0900

    rails db:xxxxxを削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,5 @@ set -e
 # pidファイルを削除する
 rm -f /tea_party/tmp/pids/server.pid && bundle exec puma -C config/puma.rb
 
-# db:createとdb:seedはfagateの初回起動時のみ実行
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
-
 # DockerfileのCMDで渡された`rails s -b 0.0.0.0`を実行
 exec "$@"
