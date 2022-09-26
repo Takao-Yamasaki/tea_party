d3fbe62df64ae8ef62b7d152d8419e63f1b517dc
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-09-27 07:51:42 +0900

    mapsテーブルの削除

---
+++ db/migrate/20220926224636_drop_maps.rb
@@ -0,0 +1,13 @@
+class DropMaps < ActiveRecord::Migration[6.1]
+  def up
+    drop_table :maps
+  end
+  def down
+    create_table "maps", force: :cascade do |t|
+      t.string "address"
+      t.float "latitude"
+      t.float "longitude"
+      t.timestamps
+    end
+  end
+end
