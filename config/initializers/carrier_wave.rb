bc22f4c8fdb8a67a8340044924ef76eb5a3e5f45
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-10-01 22:48:07 +0900

    画像アップロードで本番環境と開発環境を分ける

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,17 +3,23 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = 'tearip-backet'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tearip-backet'
-  config.fog_public = true
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    # 本番環境
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'tearip-backet'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tearip-backet'
+    config.fog_public = true
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
+  else
+    # 開発環境
+    config.storage = :file
+    # test時に処理をスキップ
+    config.enabled_processing = false if Rails.env.test?
+  end
 end
