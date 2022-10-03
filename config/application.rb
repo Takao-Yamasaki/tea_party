73a96db0a638d8c735a31e54e8b9d638e115cf72
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-10-03 18:23:36 +0900

    エラーメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Teaparty
     config.load_defaults 6.1
     # デフォルトのロケールを日本に設定
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # タイムゾーンを日本時間に設定
     config.time_zone = "Asia/Tokyo"
 
