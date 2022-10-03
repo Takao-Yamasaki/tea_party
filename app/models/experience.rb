adc91660ce9809a4bcf658c49fd12bcbe9830f9c
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-10-03 18:29:41 +0900

    rubocopによる修正

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -8,15 +8,15 @@ class Experience < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  validates :title, presence: true, length: { maximum: 30 }
-  validates :fee, presence: true, length: { maximum: 10 }
-  validates :prefecture, presence: true, length: { maximum: 10 }
-  validates :region, presence: true, length: { maximum: 10 }
-  validates :content, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: {maximum: 30}
+  validates :fee, presence: true, length: {maximum: 10}
+  validates :prefecture, presence: true, length: {maximum: 10}
+  validates :region, presence: true, length: {maximum: 10}
+  validates :content, presence: true, length: {maximum: 100}
   validates :start_datetime, presence: true
   validates :finish_datetime, presence: true
-  validates :language, presence: true, length: { maximum: 10 }
-  validates :address, presence: true, length: { maximum: 30 }
+  validates :language, presence: true, length: {maximum: 10}
+  validates :address, presence: true, length: {maximum: 30}
   validates :latitude, presence: true
   validates :longitude, presence: true
 
