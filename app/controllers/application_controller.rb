f8267f7c8111c3e49ae1e8932f21b5f3a8f3c65b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-10-04 14:12:47 +0900

    メッセージ表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :success, :danger
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :image])
   end
 
-  # def authenticate_user
-  #   if @current_user == nill
-  #     flash[:notice] = "ログインが必要です"
-  #     redirect_to("/login")
-  #   end
-  # end
 end
